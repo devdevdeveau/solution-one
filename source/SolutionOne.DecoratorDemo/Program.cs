6f05f946bf3b5e771e8fc5fa5084358d357b8abe
devdevdeveau <phil@devdevdeveau.net>
2022-11-22 12:46:14 -0500

    Naive Class Explosion

--- source/SolutionOne.DecoratorDemo/Program.cs
+++ source/SolutionOne.DecoratorDemo/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class BlackCoffee
+{
+
+}
+
+internal class LargeBlackCoffee
+{
+
+}
+
+internal class SmallBlackCoffee
+{
+
+}
+
+internal class BlackCoffeeWithTwoSugars
+{
+
+}
+
+internal class LargeBlackCoffeeWithTwoSugars
+{
+
+}
+
+internal class SmallBlackCoffeeWithTwoSugars
+{
+
+}
+
+internal class BlackCoffeeWithTwoSugarsAndCream
+{
+
+}
+
+internal class LargeBlackCoffeeWithTwoSugarsAndCream
+{
+
+}
+
+internal class SmallBlackCoffeeWithTwoSugarsAndCream
+{
+
+}
+
+internal class BlackCoffeeWithTwoSugarsAndCreamAndHazelnut
+{
+
+}
+
+internal class LargeBlackCoffeeWithTwoSugarsAndCreamAndHazelnut
+{
+
+}
+
+internal class SmallBlackCoffeeWithTwoSugarsAndCreamAndHazelNut
+{
+
+}
+
+// What if we want three sugars, what if we want raspberry?
+// This naive setup rapidly falls apart!
