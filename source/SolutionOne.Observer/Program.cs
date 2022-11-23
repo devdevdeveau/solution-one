9236ae804c6cec2f924843e3b5fb447e87a40c95
devdevdeveau <phil@devdevdeveau.net>
2022-11-22 21:24:54 -0500

    Create Observable Pattern

--- source/SolutionOne.Observer/Program.cs
+++ source/SolutionOne.Observer/Program.cs
@@ -7,49 +7,88 @@ var host = Host.CreateDefaultBuilder(args)
         services.AddTransient<IHumidityAlert, HumidityAlert>();
         services.AddTransient<ITemperatureAlert, TemperatureAlert>();
         services.AddTransient<ITemperatureRepository, TemperatureRepository>();
-        services.AddTransient<TimeAndTemperatureHandler>();
-        services.AddTransient(_=> new TimeAndTemperature(DateTimeOffset.MinValue, 70d, 40d));
+        services.AddTransient(_ => new TimeAndTemperature(DateTimeOffset.MinValue, 70d, 40d));
+        
+        // Observable
+        services.AddTransient<TimeAndTemperatureObservable>();
+        
+        // Observer
+        services.AddTransient<ITimeAndTemperatureObserver, TemperatureAlertObserver>();
+        services.AddTransient<ITimeAndTemperatureObserver, HumidityAlertObserver>();
+        services.AddTransient<ITimeAndTemperatureObserver, RepositoryObserver>();
     })
     .Build();
 
 using var scope = host.Services.CreateScope();
 var provider = scope.ServiceProvider;
-var timeAndTemperatureHandler = provider.GetRequiredService<TimeAndTemperatureHandler>();
-await timeAndTemperatureHandler.HandleReading(new TimeAndTemperature(DateTimeOffset.Now, 100d, 80d));
-await timeAndTemperatureHandler.HandleReading(new TimeAndTemperature(DateTimeOffset.Now, 20d, 20d));
-await timeAndTemperatureHandler.HandleReading(new TimeAndTemperature(DateTimeOffset.Now, 30d, 70d));
+var timeAndTemperatureObservable = provider.GetRequiredService<TimeAndTemperatureObservable>();
+await timeAndTemperatureObservable.HandleReading(new TimeAndTemperature(DateTimeOffset.Now, 100d, 80d));
+await timeAndTemperatureObservable.HandleReading(new TimeAndTemperature(DateTimeOffset.Now, 20d, 20d));
+await timeAndTemperatureObservable.HandleReading(new TimeAndTemperature(DateTimeOffset.Now, 30d, 70d));
 
-internal record TimeAndTemperature(DateTimeOffset TemperatureReading, double Temperature, double Humidity);
-
-internal class TimeAndTemperatureHandler
+internal class TimeAndTemperatureObservable
 {
-    private readonly IHumidityAlert _humidityAlert;
-    private readonly ITemperatureAlert _temperatureAlert;
-    private readonly ITemperatureRepository _temperatureRepository;
+    private readonly IEnumerable<ITimeAndTemperatureObserver> _observers;
 
-    public TimeAndTemperatureHandler(IHumidityAlert humidityAlert, ITemperatureAlert temperatureAlert, ITemperatureRepository temperatureRepository)
+    public TimeAndTemperatureObservable(IEnumerable<ITimeAndTemperatureObserver> observers)
     {
-        _humidityAlert = humidityAlert;
-        _temperatureAlert = temperatureAlert;
-        _temperatureRepository = temperatureRepository;
+        _observers = observers;
     }
 
     public async Task HandleReading(TimeAndTemperature timeAndTemperature)
     {
-        List<Task> tasks = new(3);
-
-        if (_humidityAlert.ExceedThreshold(timeAndTemperature))
-        {
-            tasks.Add(_humidityAlert.Alert(timeAndTemperature));
-        }
-
-        if (_temperatureAlert.ExceedThreshold(timeAndTemperature))
-        {
-            tasks.Add(_temperatureAlert.Alert(timeAndTemperature));
-        }
-
-        tasks.Add(_temperatureRepository.Add(timeAndTemperature));
-
-        await Task.WhenAll(tasks);
+        await Task.WhenAll(_observers.Select(observer => observer.Handle(timeAndTemperature)));
     }
 }
+
+internal interface ITimeAndTemperatureObserver
+{
+    Task Handle(TimeAndTemperature timeAndTemperature);
+}
+
+internal class TemperatureAlertObserver : ITimeAndTemperatureObserver
+{
+    private readonly ITemperatureAlert _temperatureAlert;
+
+    public TemperatureAlertObserver(ITemperatureAlert temperatureAlert)
+    {
+        _temperatureAlert = temperatureAlert;
+    }
+
+    public Task Handle(TimeAndTemperature timeAndTemperature)
+    {
+        return _temperatureAlert.ExceedThreshold(timeAndTemperature) ? _temperatureAlert.Alert(timeAndTemperature) : Task.CompletedTask;
+    }
+}
+
+internal class HumidityAlertObserver : ITimeAndTemperatureObserver
+{
+    private readonly IHumidityAlert _humidityAlert;
+
+    public HumidityAlertObserver(IHumidityAlert humidityAlert)
+    {
+        _humidityAlert = humidityAlert;
+    }
+
+    public Task Handle(TimeAndTemperature timeAndTemperature)
+    {
+        return _humidityAlert.ExceedThreshold(timeAndTemperature) ? _humidityAlert.Alert(timeAndTemperature) : Task.CompletedTask;
+    }
+}
+
+internal class RepositoryObserver : ITimeAndTemperatureObserver
+{
+    private readonly ITemperatureRepository _temperatureRepository;
+
+    public RepositoryObserver(ITemperatureRepository temperatureRepository)
+    {
+        _temperatureRepository = temperatureRepository;
+    }
+
+    public Task Handle(TimeAndTemperature timeAndTemperature)
+    {
+        return _temperatureRepository.Add(timeAndTemperature);
+    }
+}
+
+internal record TimeAndTemperature(DateTimeOffset TemperatureReading, double Temperature, double Humidity);
