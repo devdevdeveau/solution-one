f4d75aa58c3d272537f5b1ade6daa529a50493be
devdevdeveau <phil@devdevdeveau.net>
2022-11-22 21:02:05 -0500

    Observer Demo Part 1

---
+++ source/SolutionOne.Observer/HumidityAlert.cs
@@ -0,0 +1,21 @@
+﻿internal class HumidityAlert : IHumidityAlert
+{
+    private readonly TimeAndTemperature _thresholdTimeAndTemperature;
+
+    public HumidityAlert(TimeAndTemperature thresholdTimeAndTemperature)
+    {
+        _thresholdTimeAndTemperature = thresholdTimeAndTemperature;
+    }
+
+    public bool ExceedThreshold(TimeAndTemperature timeAndTemperature)
+    {
+        return timeAndTemperature.Humidity > _thresholdTimeAndTemperature.Humidity;
+    }
+
+    public Task Alert(TimeAndTemperature timeAndTemperature)
+    {
+        Console.WriteLine($"At {timeAndTemperature.TemperatureReading} {timeAndTemperature.Humidity}% exceeds humidity limit of {_thresholdTimeAndTemperature.Humidity}%");
+
+        return Task.CompletedTask;
+    }
+}
---
+++ source/SolutionOne.Observer/TemperatureAlert.cs
@@ -0,0 +1,21 @@
+﻿internal class TemperatureAlert : ITemperatureAlert
+{
+    private readonly TimeAndTemperature _thresholdTimeAndTemperature;
+
+    public TemperatureAlert(TimeAndTemperature thresholdTimeAndTemperature)
+    {
+        _thresholdTimeAndTemperature = thresholdTimeAndTemperature;
+    }
+
+    public bool ExceedThreshold(TimeAndTemperature timeAndTemperature)
+    {
+        return timeAndTemperature.Temperature > _thresholdTimeAndTemperature.Temperature;
+    }
+
+    public Task Alert(TimeAndTemperature timeAndTemperature)
+    {
+        Console.WriteLine($"At {timeAndTemperature.TemperatureReading} {timeAndTemperature.Temperature}c exceeds temperature limit of {_thresholdTimeAndTemperature.Temperature}c");
+
+        return Task.CompletedTask;
+    }
+}
