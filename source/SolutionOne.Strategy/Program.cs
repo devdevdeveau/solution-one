bd004bcc9ad08e48b62b77295f14b4fdfa9cf73d
devdevdeveau <phil@devdevdeveau.net>
2022-11-23 00:46:07 -0500

    Switch Statement Strategy

--- source/SolutionOne.Strategy/Program.cs
+++ source/SolutionOne.Strategy/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const double distance = 15d;
+const double walkMph = 3.5d;
+const double bikeMph = 15d;
+const double carMph = 60d;
+
+double CalculateTravelTime(double travelDistance, double travelSpeed) => travelDistance / travelSpeed;
+
+Console.WriteLine("The travelDistance to work is 15 miles, you can (W)alk, (B)ike, or Drive a (C)ar");
+Console.WriteLine("How do you want to go to work today?");
+var key = Console.ReadKey().KeyChar;
+
+switch (char.ToUpperInvariant(key))
+{
+    case 'W':
+        Console.WriteLine($"Walking will take {TimeSpan.FromHours(CalculateTravelTime(distance, walkMph)).TotalMinutes:F2} minutes.");
+        break;
+    case 'B':
+        Console.WriteLine($"Biking will take {TimeSpan.FromHours(CalculateTravelTime(distance, bikeMph)).TotalMinutes:F2} minutes.");
+        break;
+    case 'C':
+        Console.WriteLine($"Driving will take {TimeSpan.FromHours(CalculateTravelTime(distance, carMph)).TotalMinutes:F2} minutes.");
+        break;
+
+    default:
+        Console.WriteLine($"Unsupported Selection {key}");
+        return;
+}
